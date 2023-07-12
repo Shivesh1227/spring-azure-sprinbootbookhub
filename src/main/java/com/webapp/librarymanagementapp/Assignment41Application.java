5f8d0a819c7c3f489c58b76e4acbfef0d2a5fcd4
Shivesh1227 <shiveshtrip1227@gmail.com>
2023-07-12 23:12:57 +0530

    initial

---
+++ src/main/java/com/webapp/librarymanagementapp/Assignment41Application.java
@@ -0,0 +1,17 @@
+package com.webapp.librarymanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.webapp.librarymanagementapp.dao.BookRepository;
+
+@SpringBootApplication
+public class Assignment41Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context =SpringApplication.run(Assignment41Application.class, args);
+		BookRepository bookRepository = context.getBean(BookRepository.class);
+	}
+
+}
